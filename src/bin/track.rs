fb12dfbf676655c1889b1eef7537ebb68d9ac61d
isayasone <60773892+isayasone@users.noreply.github.com>
2024-12-30 02:34:11 +0300

    feat: tracing setup

--- src/bin/track.rs
+++ src/bin/track.rs
@@ -1,13 +1,33 @@
 use std::io::Repeat;
 
 use error_stack::{Report, ResultExt};
+use tracing::{info, instrument, warn};
 use track::{error::{AppError, Suggestion}, init};
+
+
+// #[instrument]
+// fn a(arg:i8)
+// {
+//     info!("an event");
+// }
+
 
 fn main() -> Result<(), Report<AppError>> {
     init::error_reporting();
-
+    init::tracing();
+       
+    //   a(5); 
+    // warn!(" a waring");
+    // info!("an info");
+    // trace info $ RUST_LOG=warn cargo run 
     // return Err(Report::from(AppError))
     // .attach(Suggestion("do something else"))
     // .attach_printable("a  printable value")
      Ok(())
 }
+
+
+
+
+
+
